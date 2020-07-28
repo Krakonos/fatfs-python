3dcf689b5e145e9024c24e51f74fb1c10e6ba445
Ladislav Láska <krakonos@krakonos.org>
2020-07-28 21:36:50 +0200

    Reworked the wrapper interface to allow inheritance from classes in the package.

--- pyfatfs/tests/test_fatfs.py
+++ pyfatfs/tests/test_fatfs.py
@@ -5,10 +5,36 @@ from fatfs import FatFSPartition
 
 from pyfatfs.diskio import RamDisk
 
-#def test_fatfs_check():
-#    __diskio_wrapper_disks[0] = RamDisk(bytearray(512*256))
-#    ret = diskiocheck()
-#    del __diskio_wrapper_disks[0]
+class NewPartition(FatFSPartition):
+    """
+    A helper class that handles partition mounts and initialization if necessary.
+    """
+    def __init__(self, disk = None):
+        """
+        Registers new partition on a disk (must have filesystem on it already),
+        or creates a new disk image with empty filesystem.
+        """
+        if disk is not None:
+            self.disk = disk
+        else:
+            self.disk = RamDisk(bytearray(512*256))
+            FatFSPartition.__init__(self, self.disk)
+            self.mkfs()
+
+    def __enter__(self):
+        self.mount()
+
+    def __exit__(self, except_type, except_value, except_traceback):
+        self.unmount()
+
+    def storage(self):
+        return self.disk.storage
+
+    def dump(self, fname):
+        with open(fname, "wb") as fh:
+            fh.write(self.storage())
+
+
 
 def test_fatfs_open():
     disk = RamDisk(bytearray(512*256))
@@ -19,9 +45,17 @@ def test_fatfs_open():
     handle.close()
     partition.unmount()
 
+def test_fatfs_write():
+    partition = NewPartition()
+    s = "HelloWorld!"
+    written = 0
+    with partition:
+        handle = partition.open("tf2.txt", "wb")
+        written = handle.write(s)
+        handle.close()
+    partition.dump("/tmp/testpart.img")
+    assert len(s) == written
+
 def test_diskio():
     disk = RamDisk(bytearray(512*256))
     fatfs.check_diskio(disk)
-
-#    with open("/tmp/fatfs.img", "wb") as fh:
-#        fh.write(disk.storage)
--- src/fatfs.pyx
+++ src/fatfs.pyx
@@ -104,34 +104,193 @@ cdef DWORD get_fattime():
 # int ff_del_syncobj (FF_SYNC_t sobj);	/* Delete a sync object */
 # #endif
 
+cdef class FIL_Handle:
+    cdef FIL *fp
+    def __cinit__(self):
+        self.fp = <FIL*> PyMem_Malloc(sizeof(FIL))
+
+    def __dealloc__(self):
+        PyMem_Free(self.fp)
+
+cdef class FATFS_Handle:
+    cdef FATFS* fp
+    def __cinit__(self):
+        self.fp = <FATFS*> PyMem_Malloc(sizeof(FATFS))
+
+    def __dealloc__(self):
+        PyMem_Free(self.fp)
+
+
+# Open or create a file
+def pyf_open (FIL_Handle fph, const TCHAR* path, BYTE mode) -> FRESULT:
+    return f_open(fph.fp, path, mode)
+
+# Close an open file object
+def pyf_close (FIL_Handle fph) -> FRESULT:
+    return f_close(fph.fp)
+
+# Read data from the file
+#def pyf_read (FIL* fp, void* buff, UINT btr, UINT* br) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Write data to the file
+def pyf_write (FIL_Handle fph, data) -> FRESULT:
+    assert isinstance(data, (bytes, bytearray))
+    cdef UINT written
+    cdef char* dataptr = data
+    ret = f_write(fph.fp, <void*>dataptr, len(data), &written)
+    if ret != FR_OK:
+        raise FatFSException("FatFS::close failed with error code %s" % ret)
+    assert((ret != FR_OK) or (written == len(data)), "FatFS::write succeeded, but written different %i bytes out of %i." % (written, len(data)))
+    return ret, written
+## Move file pointer of the file object
+#def pyf_lseek (FIL* fp, FSIZE_t ofs) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Truncate the file
+#def pyf_truncate (FIL* fp) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Flush cached data of the writing file
+#def pyf_sync (FIL* fp) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Open a directory
+#def pyf_opendir (DIR* dp, const TCHAR* path) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Close an open directory
+#def pyf_closedir (DIR* dp) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Read a directory item
+#def pyf_readdir (DIR* dp, FILINFO* fno) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Find first file
+#def pyf_findfirst (DIR* dp, FILINFO* fno, const TCHAR* path, const TCHAR* pattern) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Find next file
+#def pyf_findnext (DIR* dp, FILINFO* fno) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Create a sub directory
+#def pyf_mkdir (const TCHAR* path) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Delete an existing file or directory
+#def pyf_unlink (const TCHAR* path) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Rename/Move a file or directory
+#def pyf_rename (const TCHAR* path_old, const TCHAR* path_new) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Get file status
+#def pyf_stat (const TCHAR* path, FILINFO* fno) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Change attribute of a file/dir
+#def pyf_chmod (const TCHAR* path, BYTE attr, BYTE mask) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Change timestamp of a file/dir
+#def pyf_utime (const TCHAR* path, const FILINFO* fno) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Change current directory
+#def pyf_chdir (const TCHAR* path) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Change current drive
+#def pyf_chdrive (const TCHAR* path) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Get current directory
+#def pyf_getcwd (TCHAR* buff, UINT len) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Get number of free clusters on the drive
+#def pyf_getfree (const TCHAR* path, DWORD* nclst, FATFS** fatfs) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Get volume label
+#def pyf_getlabel (const TCHAR* path, TCHAR* label, DWORD* vsn) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Set volume label
+#def pyf_setlabel (const TCHAR* label) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Forward data to the stream
+#def pyf_forward (FIL* fp, UINT(*func)(const BYTE*,UINT), UINT btf, UINT* bf) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Allocate a contiguous block to the file
+#def pyf_expand (FIL* fp, FSIZE_t fsz, BYTE opt) -> FRESULT:
+#    raise Exception("Not implemented.")
+# Mount/Unmount a logical drive
+def pyf_mount (FATFS_Handle fph, const TCHAR* path, BYTE opt) -> FRESULT:
+    return f_mount(fph.fp, path, opt)
+
+# Create a FAT volume
+def pyf_mkfs (path, n_fat = 1, align = 0, n_root = 0, au_size = 0, workarea_size = 512) -> FRESULT:
+    """
+    Create a new FAT filesystem on volum given in path. The optional parameters
+    are passed to FATFS as is. Defaults will create filesystem with 1 FAT
+    table, auto alignment probed from backing device, automatically choose
+    number of rot entries and allocation unit size.
+    """
+    cdef char* buff = <char*> PyMem_Malloc(workarea_size)
+    cdef MKFS_PARM opt
+    opt.fmt = FM_FAT | FM_SFD
+    opt.n_fat = n_fat # 1 copy of FAT table
+    opt.align = align # auto align from lower layer
+    opt.n_root = n_root # auto number of root FAT entries
+    opt.au_size = au_size # auto
+    cdef FRESULT ret = f_mkfs(path, &opt, buff, 512)
+    PyMem_Free(buff)
+    return ret
+## Divide a physical drive into some partitions
+#def pyf_fdisk (BYTE pdrv, const LBA_t ptbl[], void* work) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Set current code page
+#def pyf_setcp (WORD cp) -> FRESULT:
+#    raise Exception("Not implemented.")
+## Put a character to the file
+#def pyf_putc (TCHAR c, FIL* fp) -> int:
+#    raise Exception("Not implemented.")
+## Put a string to the file
+#def pyf_puts (const TCHAR* str, FIL* cp) -> int:
+#    raise Exception("Not implemented.")
+## Put a formatted string to the file
+#def pyf_printf (FIL* fp, const TCHAR* str, ...) -> int:
+#    raise Exception("Not implemented.")
+## Get a string from the file
+#def pyf_gets (TCHAR* buff, int len, FIL* fp) -> str
+#    raise Exception("Not implemented.")
+
+
 from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
 
 class FatFSException(Exception):
     pass
 
-cdef class FileHandle:
-    cdef FIL *fp
-    cdef bint isopen
-    def __cinit__(self):
-        self.fp = <FIL*> PyMem_Malloc(sizeof(FIL))
+
+
+class FileHandle:
+    def __init__(self):
         self.isopen = False
+        self.fp = FIL_Handle()
 
     def close(self):
-        ret = f_close(self.fp)
+        ret = pyf_close(self.fp)
         if ret != FR_OK:
             raise FatFSException("FatFS::close failed with error code %s" % ret)
 
     def __dealloc__(self):
         if self.isopen:
             self.close()
-        PyMem_Free(self.fp)
 
-cdef class FatFSPartition:
-    cdef FATFS* fs
-    cdef int pdev
-    cdef public object pname
-    def __cinit__(self, disk):
-        self.fs = <FATFS*> PyMem_Malloc(sizeof(FATFS))
+    def write(self, data):
+        if isinstance(data, str):
+            data = bytes(data, 'ascii')
+        ret, written = pyf_write(self.fp, data)
+        if ret != FR_OK:
+            raise FatFSException("FatFS::close failed with error code %s" % ret)
+        return written
+
+    def read(self, size = -1):
+        pass
+
+
+class FatFSPartition():
+    def __init__(self, disk):
+        self.fs = FATFS_Handle()
+        self.disk = disk
+        self.pname = None
+        self.pdev = None
+
+        # Find pdev and pname
         # TODO: Can we fetch the constant directly? Or define it here? Does it have to be 10 only?
         for i in range(10): # corresponds to FF_VOLUMES in ffconf.h
             if not i in __diskio_wrapper_disks:
@@ -141,12 +300,8 @@ cdef class FatFSPartition:
                 break
             raise FatFSException("Physical disk limit reached. Please unmount some of the partitions.")
 
-
-    def __dealloc__(self):
-        PyMem_Free(self.fs)
-
     def mount(self):
-        ret = f_mount(self.fs, self.pname, 1)
+        ret = pyf_mount(self.fs, self.pname, 1)
         if ret == FR_OK:
             return True
         else:
@@ -161,19 +316,12 @@ cdef class FatFSPartition:
             raise FatFSException("FatFS::unmount failed with error code %s" % ret)
 
     def mkfs(self):
-        cdef char* buff = <char*> PyMem_Malloc(512)
-        cdef MKFS_PARM opt
-        opt.fmt = FM_FAT | FM_SFD
-        opt.n_fat = 1 # 1 copy of FAT table
-        opt.align = 0 # auto align from lower layer
-        opt.n_root = 0 # auto number of root FAT entries
-        opt.au_size = 0 # auto
-        f_mkfs(self.pname, &opt, buff, 512)
+        pyf_mkfs(self.pname)
 
     def open(self, path, mode):
         # TODO: Implement mode.
         handle = FileHandle()
-        ret = f_open(handle.fp, bytes(path, 'ascii'), FA_WRITE | FA_CREATE_ALWAYS)
+        ret = pyf_open(handle.fp, self.pname+bytes(path, 'ascii'), FA_WRITE | FA_CREATE_ALWAYS)
         if ret != FR_OK:
             raise FatFSException("FatFS::open failed with error code %s" % ret)
         handle.isopen = True
