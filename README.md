a1c62f52e0a9263aef66e5ba6493ab5ca3e85411
Ladislav Láska <krakonos@krakonos.org>
2020-07-29 15:39:24 +0200

    Added simple readme and package data to setup.py.

---
+++ README.md
@@ -0,0 +1,3 @@
+# FatFS wrapper for Python
+
+This library provides a Python wrapper around [ChaN's FatFS](http://elm-chan.org/fsw/ff/00index_e.html) library.
--- setup.py
+++ setup.py
@@ -37,12 +37,33 @@ if CYTHONIZE:
 else:
     extensions = no_cythonize(extensions)
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    version="0.0.1",
     name='fatfs',
+    version="0.0.1",
+    author="Ladislav Laska",
+    author_email="krakonos@krakonos.org",
+    description="A wrapper around ChaN's FatFS library for FAT filesystem manipulation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     ext_modules=extensions,
+    url="https://github.com/krakonos/fatfs-python",
     packages=['pyfatfs', 'pyfatfs.tests'],
+    #packages=setuptools.find_packages(),
     install_requires=['cython'],
     zip_safe=False,
+    python_requires='>=3.6', #TODO: Actual version?
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha"
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Embedded Systems',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Filesystems',
+    ],
 )
-    #package_data=["fatfs/source/ff.h"],
